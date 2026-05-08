729ce9389e31e3e65ef2d1cff1b3ee998ea8989b
鸡哥 <15251865468@163.com>
2026-05-08 16:49:30 +0800

    feat: 添加休息提醒相关图标

--- src/renderer/utils/SvgIcon/eisland-icon.ts
+++ src/renderer/utils/SvgIcon/eisland-icon.ts
@@ -100,6 +100,8 @@ export const SvgIcon = {
   PLUGIN: './svg/PLUGIN.svg',
   MINIMAX: './svg/MINIMAX.svg',
   BREAK: './svg/BREAK.svg',
+  PROLONGED_SITTING: './svg/PROLONGED_SITTING.svg',
+  DRINKING_WATER: './svg/DRINKING_WATER.svg',
 } as const;
 
 export type SvgIconKey = keyof typeof SvgIcon;
