ec8350870702939f21fb6cb10734385376b8efd8
鸡哥 <15251865468@163.com>
2026-05-11 20:30:15 +0800

    feat: 创建工具箱格式工厂空白界面

---
+++ src/renderer/components/states/maxExpand/components/tools/components/FormatFactoryToolSection.tsx
@@ -0,0 +1,37 @@
+/*
+ * eIsland - A sleek, Apple Dynamic Island inspired floating widget for Windows, built with Electron.
+ * https://github.com/JNTMTMTM/eIsland
+ *
+ * Copyright (C) 2026 JNTMTMTM
+ * Copyright (C) 2026 pyisland.com
+ *
+ * Original author: JNTMTMTM[](https://github.com/JNTMTMTM)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+/**
+ * @file FormatFactoryToolSection.tsx
+ * @description 工具箱格式工厂模块
+ * @author 鸡哥
+ */
+
+import type { ReactElement } from 'react';
+
+/**
+ * 格式工厂模块主视图。
+ */
+export function FormatFactoryToolSection(): ReactElement {
+  return (
+    <div className="settings-cards">
+    </div>
+  );
+}
