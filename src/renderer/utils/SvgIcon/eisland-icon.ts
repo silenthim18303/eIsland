64475654293e0972ea5f71c781f582518ab525ad
鸡哥 <15251865468@163.com>
2026-05-13 23:13:29 +0800

    feat: 添加通知功能配置界面矢量图标

--- src/renderer/utils/SvgIcon/eisland-icon.ts
+++ src/renderer/utils/SvgIcon/eisland-icon.ts
@@ -104,6 +104,7 @@ export const SvgIcon = {
   DRINKING_WATER: './svg/DRINKING_WATER.svg',
   SWITCHING: './svg/SWITCHING.svg',
   SOUND: './svg/SOUND.svg',
+  NOTIFICATION: './svg/NOTIFICATION.svg',
 } as const;
 
 export type SvgIconKey = keyof typeof SvgIcon;
