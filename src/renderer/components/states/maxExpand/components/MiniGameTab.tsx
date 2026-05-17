3d184c23c3bd4a95c1108058bca038b087abc6b4
鸡哥 <15251865468@163.com>
2026-05-17 23:09:06 +0800

    fix: 补齐所有 JSDoc 注释

--- src/renderer/components/states/maxExpand/components/MiniGameTab.tsx
+++ src/renderer/components/states/maxExpand/components/MiniGameTab.tsx
@@ -1,7 +1,37 @@
+/*
+ * eIsland - A sleek, Apple Dynamic Island inspired floating widget for Windows, built with Electron.
+ * https://github.com/JNTMTMTM/eIsland
+ *
+ * Copyright (C) 2026 JNTMTMTM
+ * Copyright (C) 2026 pyisland.com
+ *
+ * Original author: JNTMTMTM[](https://github.com/JNTMTMTM)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+/**
+ * @file MiniGameTab.tsx
+ * @description MaxExpand 迷你游戏占位 Tab 组件
+ * @author 鸡哥
+ */
+
 import type { ReactElement } from 'react';
 import { useTranslation } from 'react-i18next';
 import eislandLogo from '../../../../../../resources/icon/eisland.svg';
 
+/**
+ * 迷你游戏 Tab 内容
+ * @returns React 元素
+ */
 export function MiniGameTab(): ReactElement {
   const { t } = useTranslation();
 
